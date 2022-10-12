178224ecf67cfc3eae9fbcdb55672e1c4c25ad05
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2022-10-11 21:14:53 -0300

    Adicionando template do html estático dentro do componente de Pagina

--- src/components/Pagina.jsx
+++ src/components/Pagina.jsx
@@ -2,7 +2,186 @@ import React from "react"
 
 export default () => (
     <>
-        <h1>Olá, mundo!</h1>
+        <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Coloque um logo</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="#secao-dica-1">Dica 1</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Dica 2</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Cadastro</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Lista de alunos</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div className="container pb-5">
+      <div className="row">
+        <div className="col">
+          <h1 className="display-3 mb-4">
+            Este é um teste de programação Frontend
+          </h1>
+          <h3 className="mb-5">
+            Vamos fazer um cadastro básico e uma listagem
+          </h3>
+          <button type="button" className="btn btn-primary">
+            Cadastro aluno
+          </button>
+        </div>
+        <div className="col">
+          <img src="https://via.placeholder.com/1000x500?text=Coloque+uma+imagem" className="img-fluid" alt="..."/>
+        </div>
+      </div>
+    </div>
+    <div className="container bg-secondary bg-opacity-25" id="secao-dica-1">
+      <div className="row">
+        <div className="col">
+          <h1 className="display-3 mb-4 text-center">Dica 1</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col text-center">
+          <img src="https://via.placeholder.com/100x100?text=Coloque+uma+imagem" className="rounded-circle" alt="..."/>
+          <h1>
+            Os links no menu
+          </h1>
+          <h3>
+            Cada link deve navegar para uma aréa correspondente do site
+          </h3>
+        </div>
+        <div className="col text-center">
+          <img 
+            src="https://via.placeholder.com/100x100?text=Coloque+uma+imagem" 
+            className="rounded-circle" alt="..."/> 
+          <h1>
+            Os botões
+          </h1>
+          <h3>
+            Deve navegar para a aréa de cadastro de aluno
+          </h3>
+        </div>
+      </div>
+    </div>
+    <div className="container pt-5">
+      <div className="row">
+        <div className="col">
+          <img src="https://via.placeholder.com/400x300?text=Coloque+uma+imagem" className="img-fluid" alt="..."/>
+        </div>
+        <div className="col">
+          <h1 className="display-3 mb-4">
+            Dica 2
+          </h1>
+          <h3>
+            Cada aluno cadastrado deve aparecer na tabela abaixo do formulário, além da inclusão, crie um método para remover o cadastro
+          </h3>
+          <button type="button" className=" mt-3 btn btn-primary">
+            Cadastro aluno
+          </button>
+        </div>
+      </div>
+    </div>      
+    <div className="container mt-5 bg-secondary bg-opacity-25 ">
+      <div className="row">
+        <di className="col">
+          <h1 className="text-center display-3">
+            Cadastro Aluno
+          </h1>
+        </di>
+      </div>
+      <form>
+        <div className="row">
+          <div className="col">
+            <label for="id_nome" className="form-label">Nome *</label>
+            <input type="text" className="form-control" id="id_nome" 
+              placeholder="Digite o nome do aluno" required/>
+          </div>
+          <div className="col">
+            <label for="id_email" className="form-label">Email *</label>
+            <input type="email" className="form-control" id="id_email" 
+              placeholder="name@example.com" required/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col mt-3"> 
+            <label for="id_cpf" className="form-label">CPF *</label>
+            <input type="text" className="form-control" id="id_cpf" 
+              placeholder="000.000.000-00" required/>
+          </div>
+          <div className="col mt-3">
+            <label for="id_curso" className="form-label">Curso *</label>
+            <input type="text" className="form-control" id="id_curso" 
+              placeholder="Curso do aluno" required/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col text-center mt-5">
+            <button type="submit" className="btn btn-primary">
+              Cadastrar
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+    <div className="container text-center mt-5">
+      <h1>
+        Lista de Alunos
+      </h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Email</th>
+            <th scope="col">CPF</th>
+            <th scope="col">Curso</th>
+            <th scope="col">Ações</th>
+          </tr>
+        </thead>
+        <tbody className="table-group-divider">
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>Angela</td>
+            <td>
+              <button>
+                remover
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            <td>Aaa</td>
+            <td>
+              <button>
+                remover
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div className="container text-center mt-5 bg-secondary bg-opacity-25">
+      <h5>
+        Powered by Mach1 - Pilotando para o futuro!
+      </h5>
+
+    </div>
     </>
 )
 
